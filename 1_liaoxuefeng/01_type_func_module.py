a80e674e41b054bd35d2fcf9b12dbc5db8c2569a
Yu,Haipeng <hpyu@live.cn>
2016-05-31 07:38:37 +0800

    liaoxuefeng git tutorial init
    add: 1st templet for basic type, func, module

---
+++ 1_liaoxuefeng/01_type_func_module.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+
+
+def main():
+    printf("I love python!")
+    
+
+if __name__ == '__main__':
+    from distutils.log import warn as printf
+    main()
+
--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # python_learning
 This repo record code snippets from python books for python ramp-up
+
+## www.liaoxuefeng.com 《Git 教程》
+
